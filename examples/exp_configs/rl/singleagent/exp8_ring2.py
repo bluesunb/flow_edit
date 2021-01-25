d124b47600f1e7ce2d8096fb97b026e1e3a11ec1
bmil06 <bmil06@>
2021-01-25 17:51:45 +0900

    experiments 추가

--- examples/exp_configs/rl/singleagent/exp8_ring2.py
+++ examples/exp_configs/rl/singleagent/exp8_ring2.py
@@ -2,7 +2,7 @@
 from flow.core.params import VehicleParams, SumoCarFollowingParams, SumoLaneChangeParams
 from flow.core.params import SumoParams, EnvParams, InitialConfig, NetParams
 from flow.controllers import RLController, IDMController, ContinuousRouter, SimLaneChangeController
-from flow.envs import WaveAttenuationEnv, LaneChangeAccelEnv, MyLaneChangeAccelEnv
+from flow.envs import WaveAttenuationEnv, LaneChangeAccelEnv, MyLaneChangeAccelEnv, LaneChangeAccelPOEnv
 from flow.networks import RingNetwork
 
 import os
@@ -22,7 +22,7 @@ vehicles.add(
     veh_id='outline',
     acceleration_controller=(IDMController, {'v0': 2}),
     routing_controller=(ContinuousRouter, {}),
-    initial_speed=3,
+    initial_speed=2,
     num_vehicles=6,
     car_following_params=SumoCarFollowingParams(
         speed_mode='aggressive',
@@ -30,33 +30,18 @@ vehicles.add(
     )
 )
 
-# vehicles.add(
-#     veh_id='inline',
-#     acceleration_controller=(IDMController, {'v0': 2}),
-#     routing_controller=(ContinuousRouter, {}),
-#     initial_speed=3,
-#     num_vehicles=3,
-#     car_following_params=SumoCarFollowingParams(
-#         speed_mode='aggressive',
-#         min_gap=0
-#     )
-# )
 
 vehicles.add(
     veh_id='rl',
-    acceleration_controller=(IDMController, {}),
+    acceleration_controller=(RLController, {}),
     routing_controller=(ContinuousRouter, {}),
-    lane_change_params=SumoLaneChangeParams(
-        lane_change_mode=11108,
-        lc_speed_gain=10,
-    ),
     initial_speed=5,
     num_vehicles=1,
 )
 
 flow_params = dict(
     exp_tag=current_file_name,
-    env_name=LaneChangeAccelEnv,
+    env_name=MyLaneChangeAccelEnv,
     network=RingNetwork,
     simulator='traci',
     sim=SumoParams(
@@ -68,7 +53,7 @@ flow_params = dict(
     env=EnvParams(
         horizon=HORIZON,
         warmup_steps=750,
-        clip_actions=True,
+        clip_actions=False,
         additional_params={
             "max_accel": 3,
             "max_decel": 3,
@@ -97,10 +82,10 @@ flow_params = dict(
         },
         reward_params={
             'only_rl': True,
-            'simple_lc_penalty': 0.1,
+            'simple_lc_penalty': 0.3,
             'unnecessary_lc_penalty': (0,0),
             'max_lc_headway': 0,
-            'dc2_penalty': 0,
+            'dc2_penalty': 0.2,
             'overtake_reward': 0,
-
-},),)
+            'unsafe_penalty': 0.3,
+		},),)
