b1df37d36b3c959bd5377e316f2817c9d383c9f0
bmil10 <bmil10@>
2021-02-19 19:25:54 +0900

    ring networks add new spacing

--- flow/networks/ring.py
+++ flow/networks/ring.py
@@ -4,6 +4,7 @@ from flow.networks.base import Network
 from flow.core.params import InitialConfig
 from flow.core.params import TrafficLightParams
 from numpy import pi, sin, cos, linspace
+import numpy as np
 
 ADDITIONAL_NET_PARAMS = {
     # length of the ring road
@@ -214,3 +215,42 @@ class RingNetwork(Network):
                       (":bottom_0", ring_length + 3 * junction_length)]
 
         return edgestarts
+
+    @staticmethod
+    def gen_custom_start_pos(cls, net_params, initial_config, num_vehicles):
+        VEHICLE_LENGTH = 5
+
+        length = net_params.additional_params['length']
+        num_lane = net_params.additional_params['lanes']
+        min_gap = initial_config.min_gap
+        my_min_gap = min_gap + 23
+        startpos, startlane = [], []
+
+        if length < num_vehicles * (VEHICLE_LENGTH + my_min_gap):
+            raise ValueError('num of vehicles are too many')
+
+        surplus_gap = length - num_vehicles * (VEHICLE_LENGTH + my_min_gap)
+        tmp = list(range(int(num_vehicles//2+num_vehicles%2))) + list(reversed(range(int(num_vehicles//2))))
+        surplus_gap_list = np.array(tmp)/sum(tmp) * surplus_gap
+
+        edges = ['bottom', 'right', 'top', 'left']
+        edge_length = length // 4
+
+        startpos.append((edges[0], 0))
+        startlane.append(0)
+
+        lane_index = list(range(0,num_lane, 2))+list(range(1, num_lane, 2))
+        startlane += [lane_index[i%len(lane_index)] for i in range(num_vehicles-1)]
+
+        pos = 0
+        for veh, gap in zip(range(num_vehicles - 1), surplus_gap_list):
+            pos = pos + my_min_gap + VEHICLE_LENGTH + gap
+            startpos.append((edges[int(pos // edge_length)], pos % edge_length))
+            # startlane.append((startlane[-1] + 1+int(num_lane>2)) % num_lane)
+        else:
+            rl_pos = startpos.pop(0)
+            rl_lane = startlane.pop(0)
+            startpos.append(rl_pos)
+            startlane.append(rl_lane)
+
+        return startpos, startlane
